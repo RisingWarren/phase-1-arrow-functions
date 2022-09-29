6d4e99bdd5eec9eeea428b33a0433695a3c56ace
Warren Santiago <wsantiago9623@gmail.com>
2022-09-29 12:19:56 -0400

    commit tree b46e637f17c973582d0ef3eb7365b208f49f3579

---
+++ index.js
@@ -0,0 +1,9 @@
+const divide = (parameter1, parameter2) => (parameter1/parameter2);
+let parameter1 = 2000
+let parameter2 = 100
+
+const square = prm1 => (prm1*prm1)
+let prm1 = 2
+
+const add = (prm2,prm3) => (prm2 + prm3)
+
